2c8adfd6a0be4c00406d40887bc0a378aeef7ada
SpySparsh <153936815+SpySparsh@users.noreply.github.com>
2025-11-07 22:33:00 +0530

    Update MongoDB connection string to use ai_collab

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 async function connect() {
     try {
-        await mongoose.connect("mongodb+srv://spyspring30_db_user:Ib2dgkr3UfDjM0kw@cluster0.ujeduvy.mongodb.net/", {
+        await mongoose.connect("mongodb+srv://spyspring30_db_user:Ib2dgkr3UfDjM0kw@cluster0.ujeduvy.mongodb.net/ai_collab", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
